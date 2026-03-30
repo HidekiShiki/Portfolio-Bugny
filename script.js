3e16e06a4e5509aea9b954505386aa3b39a80cfa
HidekiShiki <jp.hidekifk@gmail.com>
2026-03-30 12:30:38 -0300

    Fourth Commit | Quarto Commit
    Adding JavaScript for sending messages via WhatsApp.
    
    Adicionando JavaScript para envio de mensagem via WhatsApp.

---
+++ script.js
@@ -0,0 +1,21 @@
+function enviarWhats(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const message = document.getElementById('message').value.trim();
+    if (!name || !message) {
+        alert('Por favor, preencha ambos os campos: Nome e Mensagem.');
+        return;
+    }
+    
+    const telefone = '55 35 98435-9815';
+    const text = `Olá, meu nome é ${name},  ${message}`;
+    const msgFormatada = encodeURIComponent(text);
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+// });
